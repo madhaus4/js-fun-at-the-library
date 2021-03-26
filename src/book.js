a4570212934fa1f749b89049a2d7397166f6ae7a
Maria DelSignore <maria.ad27@gmail.com>
2021-03-26 08:50:05 -0600

    Complete book file

--- src/book.js
+++ src/book.js
@@ -1,12 +1,58 @@
-function createTitle() {
-  
-}
+// function createTitle() {
+//   'Oath of Sunshine';
+// }
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
 }
+
+function createTitle(title) {
+  return `The ${title}`;
+}
+
+function buildMainCharacter(name, age, pronouns) {
+  var character = {
+    name: name,
+    age: age,
+    pronouns: pronouns,
+  }
+  return character;
+}
+
+function saveReview(newReview, reviews) {
+  if (!reviews.length) {
+    reviews.push(newReview);
+  }
+  for (var i = 0; i < reviews.length; i++) {
+    if (newReview === reviews[i]) {
+      return reviews;
+    } else {
+      reviews.push(newReview);
+      return reviews;
+    }
+  }
+};
+
+function calculatePageCount(bookTitle, bookPageCount) {
+  return bookTitle.length * 20;
+};
+
+function writeBook(bookTitle, bookCharacter, genre) {
+  var book = {
+    title: bookTitle,
+    mainCharacter: bookCharacter,
+    pageCount: calculatePageCount(bookTitle),
+    genre: genre,
+  };
+  return book;
+};
+
+function editBook(book) {
+  book.pageCount = book.pageCount * .75
+  return book.pageCount;
+};
