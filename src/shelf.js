13558b456ca3632dd163e7d25f1da95f2db76138
Maria DelSignore <maria.ad27@gmail.com>
2021-03-27 15:56:46 -0600

    Complete shelf file

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,46 @@
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  shelfBook,
+  unshelfBook,
+  listTitles,
+  searchShelf
 };
+
+function shelfBook(book, shelf) {
+  if (shelf.length < 3) {
+    return shelf.unshift(book);
+  }
+};
+
+function unshelfBook(book, shelf) {
+  for (var i = 0; i < shelf.length; i++) {
+    if (book === shelf[i].title) {
+    shelf.splice(i, 1);
+    }
+  }
+  return shelf;
+};
+
+function listTitles(shelf) {
+  var shelfTitles = [];
+  for (var i = 0; i < shelf.length; i++) {
+  shelfTitles.push(shelf[i].title);
+  }
+  return shelfTitles.join(', ');
+};
+
+function searchShelf(shelf, title) {
+  for (var i = 0; i < shelf.length; i++) {
+    if (shelf[i].title === title) {
+      return true;
+    }
+  }
+  return false;
+};
+
+//
+//
+//
+//
+//
+//
